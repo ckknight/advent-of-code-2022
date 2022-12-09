5889c973bac0db945e57895ddfd1afebaa27efb3
Cameron Knight <cameron.knight@bumped.com>
2022-12-09 08:29:09 -0800

    day 8

--- run.mjs
+++ run.mjs
@@ -1,26 +1,8 @@
-import { day07 } from "./src/day07.mjs";
+import { day08 } from "./src/day08.mjs";
 
-const input = `$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k`
-console.log(day07(input));
+const input = `30373
+25512
+65332
+33549
+35390`
+console.log(day08(input));
