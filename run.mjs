e6bbfdf94ded0aeb1d7e64f729f35e5cae9030fd
Cameron Knight <cameron.knight@bumped.com>
2022-12-12 18:25:54 -0800

    day 12

--- run.mjs
+++ run.mjs
@@ -1,30 +1,8 @@
-import { day11 } from "./src/day11.mjs";
+import { day12 } from "./src/day12.mjs";
 
-const input = `Monkey 0:
-Starting items: 79, 98
-Operation: new = old * 19
-Test: divisible by 23
-  If true: throw to monkey 2
-  If false: throw to monkey 3
-
-Monkey 1:
-Starting items: 54, 65, 75, 74
-Operation: new = old + 6
-Test: divisible by 19
-  If true: throw to monkey 2
-  If false: throw to monkey 0
-
-Monkey 2:
-Starting items: 79, 60, 97
-Operation: new = old * old
-Test: divisible by 13
-  If true: throw to monkey 1
-  If false: throw to monkey 3
-
-Monkey 3:
-Starting items: 74
-Operation: new = old + 3
-Test: divisible by 17
-  If true: throw to monkey 0
-  If false: throw to monkey 1`
-console.log(day11(input));
+const input = `Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi`
+console.log(day12(input));
